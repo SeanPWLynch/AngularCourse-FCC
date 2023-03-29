514e4040294b9e67b6849f4b33ac1fedc515944e
Sean Lynch <sean.p.w.lynch@gmail.com>
2023-03-29 22:16:43 +0100

    started types lesson

--- TypeScriptDemo/datatypes.ts
+++ TypeScriptDemo/datatypes.ts
@@ -1,4 +1,57 @@
-let fname='Sean'
+let fname : string;
 
-fname =10
- 
+fname = "Sean";
+
+let newname = fname.toUpperCase();
+
+console.log(newname);
+
+
+let age: number;
+age=32;
+
+let ageString = "32"
+
+let inferAge = parseInt(ageString);
+
+console.log(inferAge);
+
+let isValid: boolean;
+
+isValid = false;
+
+console.log(isValid);
+
+let empList : string[];
+
+empList = ["Matthew","Mark","John"];
+
+console.log(empList);
+
+let numList : Array<number>;
+
+numList = [1,2,3,4,5];
+
+let filterResult = numList.filter(num => num > 2);
+
+console.log(numList);
+console.log(filterResult);
+
+
+let sum = numList.reduce((acc,num) => acc+num);
+
+console.log(sum);
+
+enum Colour {
+    Red,
+    Green,
+    Blue
+}
+
+let testRed = Colour.Red;
+let testGreen = Colour.Green;
+let testBlue = Colour.Blue;
+
+console.log(testRed);
+console.log(testGreen);
+console.log(testBlue);
