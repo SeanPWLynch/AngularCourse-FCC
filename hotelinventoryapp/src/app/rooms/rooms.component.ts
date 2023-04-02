647883cca18e1098ddfc412d1b454f58805aec7e
Sean Lynch <sean.p.w.lynch@gmail.com>
2023-04-02 18:19:44 +0100

    basic angular binding

--- hotelinventoryapp/src/app/rooms/rooms.component.ts
+++ hotelinventoryapp/src/app/rooms/rooms.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'hinv-rooms',
   templateUrl: './rooms.component.html',
   styleUrls: ['./rooms.component.scss']
 })
-export class RoomsComponent {
+export class RoomsComponent implements OnInit{
 
+  hotelName = 'Hilton Hotels';
+  numberOfRooms = 100;
+
+  hiderooms = false
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  toggle() {
+    this.hiderooms = !this.hiderooms;
+  }
 
 
 }
