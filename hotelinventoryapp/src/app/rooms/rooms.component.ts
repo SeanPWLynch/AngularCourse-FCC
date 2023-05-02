7b640790b1c41231a9cc04e7b5d5cf8d1aa1df95
Sean Lynch <sean.p.w.lynch@gmail.com>
2023-05-02 21:07:54 +0100

    after view checked, 6 hour mark in course

--- hotelinventoryapp/src/app/rooms/rooms.component.ts
+++ hotelinventoryapp/src/app/rooms/rooms.component.ts
@@ -1,12 +1,21 @@
-import { Component, DoCheck, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  DoCheck,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { Room, RoomList } from './rooms';
+import { HeaderComponent } from '../header/header.component';
+import { AfterViewChecked } from '@angular/core';
 
 @Component({
   selector: 'hinv-rooms',
   templateUrl: './rooms.component.html',
   styleUrls: ['./rooms.component.scss'],
 })
-export class RoomsComponent implements OnInit, DoCheck {
+export class RoomsComponent implements OnInit, DoCheck, AfterViewInit, AfterViewChecked {
+  @ViewChild(HeaderComponent) headerComponent!: HeaderComponent;
 
   selectedRoom!: RoomList;
 
@@ -15,15 +24,25 @@ export class RoomsComponent implements OnInit, DoCheck {
     console.log(room);
   }
 
-
   roomList: RoomList[] = [];
   constructor() {}
 
+  ngAfterViewChecked(): void {
+    this.headerComponent.title= "Rooms View After View Checked";
+
+    console.log("After View Checked")
+  }
+  ngAfterViewInit(): void {
+  //console.log(this.headerComponent)
+  this.headerComponent.title= "Rooms View";
+  }
+
   ngDoCheck(): void {
-    console.log('On Changes Is Called');
+    //console.log('On Changes Is Called');
   }
 
   ngOnInit(): void {
+    console.log(this.headerComponent);
     this.roomList = [
       {
         roomNumber: 1,
@@ -83,29 +102,26 @@ export class RoomsComponent implements OnInit, DoCheck {
     totalRooms: 20,
   };
 
-  title : string = 'Room List';
+  title: string = 'Room List';
 
   toggle() {
     this.hiderooms = !this.hiderooms;
-    this.title = 'Rooms List'
+    this.title = 'Rooms List';
   }
 
-  addRoom(){
-     const newRoom : RoomList = {
-        roomNumber: 5,
-        roomType: 'Deluxe Room',
-        amenities: 'Aircon, Wifi ,TV, Bathroom, Kitchen',
-        price: 1000,
-        photos:
-          'https://assets-global.website-files.com/5c6d6c45eaa55f57c6367749/624b471bdf247131f10ea14f_61d31b8dbff9b500cbd7ed32_types_of_rooms_in_a_5-star_hotel_2_optimized_optimized.jpeg',
-        checkInTime: new Date('17-April-2023'),
-        checkOutTime: new Date('18-April-2023'),
-        rating: 4.4,
-      }
+  addRoom() {
+    const newRoom: RoomList = {
+      roomNumber: 5,
+      roomType: 'Deluxe Room',
+      amenities: 'Aircon, Wifi ,TV, Bathroom, Kitchen',
+      price: 1000,
+      photos:
+        'https://assets-global.website-files.com/5c6d6c45eaa55f57c6367749/624b471bdf247131f10ea14f_61d31b8dbff9b500cbd7ed32_types_of_rooms_in_a_5-star_hotel_2_optimized_optimized.jpeg',
+      checkInTime: new Date('17-April-2023'),
+      checkOutTime: new Date('18-April-2023'),
+      rating: 4.4,
+    };
 
-      this.roomList = [...this.roomList,newRoom]
-
-    }
-
-
+    this.roomList = [...this.roomList, newRoom];
   }
+}
