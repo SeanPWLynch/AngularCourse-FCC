7b31feb5c8191db92d6848fbadbf86ba52eb03fc
Sean Lynch <sean.p.w.lynch@gmail.com>
2023-05-04 15:51:44 +0100

    DI Resolutions

--- hotelinventoryapp/src/app/rooms/rooms.component.ts
+++ hotelinventoryapp/src/app/rooms/rooms.component.ts
@@ -11,6 +11,7 @@ import {
 import { Room, RoomList } from './rooms';
 import { HeaderComponent } from '../header/header.component';
 import { AfterViewChecked } from '@angular/core';
+import { RoomsService } from './services/rooms.service';
 
 @Component({
   selector: 'hinv-rooms',
@@ -20,11 +21,23 @@ import { AfterViewChecked } from '@angular/core';
 export class RoomsComponent
   implements OnInit, DoCheck, AfterViewInit, AfterViewChecked
 {
+  //roomService = new RoomsService();
+
+  constructor(private roomsService: RoomsService) {
+
+  }
+
+  ngOnInit(): void {
+    this.roomList = this.roomsService.getRooms();
+  }
+
   @ViewChild(HeaderComponent) headerComponent!: HeaderComponent;
   @ViewChildren(HeaderComponent)
   headerChildrenComponent!: QueryList<HeaderComponent>;
 
   selectedRoom!: RoomList;
+
+
 
   selectRoom(room: RoomList) {
     this.selectedRoom = room;
@@ -32,7 +45,6 @@ export class RoomsComponent
   }
 
   roomList: RoomList[] = [];
-  constructor() {}
 
 
   ngAfterViewChecked(): void {
@@ -47,55 +59,7 @@ export class RoomsComponent
     //console.log('On Changes Is Called');
   }
 
-  ngOnInit(): void {
-    console.log(this.headerComponent);
-    this.roomList = [
-      {
-        roomNumber: 1,
-        roomType: 'Deluxe Room',
-        amenities: 'Aircon, Wifi ,TV, Bathroom, Kitchen',
-        price: 500,
-        photos:
-          'https://assets-global.website-files.com/5c6d6c45eaa55f57c6367749/624b471bdf247131f10ea14f_61d31b8dbff9b500cbd7ed32_types_of_rooms_in_a_5-star_hotel_2_optimized_optimized.jpeg',
-        checkInTime: new Date('02-April-2023'),
-        checkOutTime: new Date('03-April-2023'),
-        rating: 4.5,
-      },
-      {
-        roomNumber: 2,
-        roomType: 'Single Room',
-        amenities: 'Aircon, Wifi ,TV, Bathroom, Kitchen',
-        price: 300,
-        photos:
-          'https://media.istockphoto.com/id/1044040182/photo/bedroom-interior.jpg?s=612x612&w=0&k=20&c=qbu-2lryG55jgj5zZ-eiM4Q5UhdOiXoHBDTIBJ5SqyE=',
-        checkInTime: new Date('02-April-2023'),
-        checkOutTime: new Date('03-April-2023'),
-        rating: 3.7,
-      },
-      {
-        roomNumber: 31,
-        roomType: 'Executive Room',
-        amenities: 'Aircon, Wifi ,TV, Bathroom, Kitchen',
-        price: 1000,
-        photos:
-          'https://img1.10bestmedia.com/Images/Photos/378649/Park-Hyatt-New-York-Manhattan-Sky-Suite-Master-Bedroom-low-res_54_990x660.jpg',
-        checkInTime: new Date('02-April-2023'),
-        checkOutTime: new Date('13-April-2023'),
-        rating: 4.52,
-      },
-      {
-        roomNumber: 4,
-        roomType: 'Deluxe Room',
-        amenities: 'Aircon, Wifi ,TV, Bathroom, Kitchen',
-        price: 500,
-        photos:
-          'https://assets-global.website-files.com/5c6d6c45eaa55f57c6367749/624b471bdf247131f10ea14f_61d31b8dbff9b500cbd7ed32_types_of_rooms_in_a_5-star_hotel_2_optimized_optimized.jpeg',
-        checkInTime: new Date('02-April-2023'),
-        checkOutTime: new Date('03-April-2023'),
-        rating: 2.2,
-      },
-    ];
-  }
+
 
   hotelName = 'Hilton Hotels';
 
